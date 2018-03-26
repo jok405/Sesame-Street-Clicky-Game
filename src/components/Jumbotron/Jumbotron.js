15873285f3ee06799a937b3fea2020baf0934f21
jok405 <john.y.kim84@gmail.com>
2018-03-25 23:24:39 -0400

    adding background

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = props => (
+    <div className="jumbotron">
+        <h1>Click a Sesame Street muppet to earn points, but avoid clicking the same image twice!</h1>
+    </div>
+);
+
+export default Jumbotron;
