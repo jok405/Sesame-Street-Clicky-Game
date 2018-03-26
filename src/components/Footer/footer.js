b8ed4d172c16823744f8ab3424c089c8d2ccc223
jok405 <john.y.kim84@gmail.com>
2018-03-25 23:25:18 -0400

    adding footer

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => (
+    <div>
+        <div className="footer">
+            <div className="text-center">
+                Copyright &copy; John Kim | 2018
+            </div>
+        </div>
+    </div>
+)
+
+export default Footer;
